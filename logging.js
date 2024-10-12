3679995f764a493dc0390f660932ab7c883cf33e
Thotino GOBIN-GANSOU <gthomario@yahoo.com>
2024-10-12 18:17:52 +0200

    feat: logging with winston

---
+++ logging.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const winston = require("winston");
+const logger = winston.createLogger({
+  level: "info",
+  transports: [new winston.transports.Console()],
+});
+
+module.exports = logger;
